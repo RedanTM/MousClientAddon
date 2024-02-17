9d56e842606743936f4773dc48ee3844bdbb3ffe
n0rzik <124731778+n0rzik@users.noreply.github.com>
2024-02-17 19:33:05 +0300

    Update Mous.java

--- src/main/java/RedanTM/n0rzik/Mous.java
+++ src/main/java/RedanTM/n0rzik/Mous.java
@@ -1,43 +1,42 @@
 package RedanTM.n0rzik;
 
 import RedanTM.n0rzik.modules.*;
-import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import net.fabricmc.loader.impl.util.log.Log;
-import org.slf4j.Logger;
 
 public class Mous extends MeteorAddon {
-    public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("MousClientAddon");
-    public static final HudGroup HUD_GROUP = new HudGroup("MousClientAddon");
-    public static final String LOGPREFIX = "[MousClient]";
+    public static final Category CATEGORY = new Category("MousClient");
 
     @Override
     public void onInitialize() {
-        LOG.info(LOGPREFIX + " intiliazing module.....");
-        // Modules
-        Modules module = Modules.get();
-        Modules.get().add(new AutoSwim());
-        Modules.get().add(new Clip());
-        Modules.get().add(new JoinMessage());
-        Modules.get().add(new PortalGodMode());
-        Modules.get().add(new HoleSnap());
-        Modules.get().add(new NoClearChat());
-        Modules.get().add(new PistonPush());
-        Modules.get().add(new SurroundPlus());
+        Modules.get().add(new AntiBot()); //petr1shka
+        Modules.get().add(new AutoCrystalRewrite()); //n0rzik
+        Modules.get().add(new AutoSwim()); //petr1shka
+        Modules.get().add(new BedAuraPlus()); //n0rzik
+        Modules.get().add(new BurrowNotification()); //petr1shka
+        Modules.get().add(new ChatEmotes()); //petr1shka
+        Modules.get().add(new ChatPrefixPlus()); //petr1shka
+        Modules.get().add(new EzPop()); //petr1shka
+        Modules.get().add(new FastFall()); //petr1shka
+        Modules.get().add(new HoleSnapPlus()); //n0rzik
+        Modules.get().add(new JoinMessage()); //n0rzik
+        Modules.get().add(new NoClearChat()); //n0rzik
+        Modules.get().add(new PingSpoofer()); //petr1shka
+        Modules.get().add(new PistonPush()); //n0rzik
+        Modules.get().add(new RPC()); //n0rzik
+        Modules.get().add(new ScaffoldPlus()); //petr1shka
+        Modules.get().add(new SurroundPlus()); //n0rzik
+        Modules.get().add(new SelfTrapPlus()); //n0rzik
     }
 
     @Override
     public void onRegisterCategories() {
-        LOG.info(LOGPREFIX + " refister categories.....");
         Modules.registerCategory(CATEGORY);
     }
 
     @Override
     public String getPackage() {
-        return "com.example.addon";
+        return "RedanTM.n0rzik";
     }
 }
