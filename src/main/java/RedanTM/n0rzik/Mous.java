0d96bf67c5eeef19ddb759639bd0a8f8baff0eb0
petr1shka <144609881+petr1shka@users.noreply.github.com>
2024-02-17 12:18:16 +0300

    Update Mous.java

--- src/main/java/RedanTM/n0rzik/Mous.java
+++ src/main/java/RedanTM/n0rzik/Mous.java
@@ -6,18 +6,21 @@ import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.fabricmc.loader.impl.util.log.Log;
 import org.slf4j.Logger;
 
 public class Mous extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("MousClientAddon");
     public static final HudGroup HUD_GROUP = new HudGroup("MousClientAddon");
+    public static final String LOGPREFIX = "[MousClient]";
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing MousClientAddon");
-
+        LOG.info(LOGPREFIX + " intiliazing module.....");
         // Modules
+        Modules module = Modules.get();
+        Modules.get().add(new AutoSwim());
         Modules.get().add(new Clip());
         Modules.get().add(new JoinMessage());
         Modules.get().add(new PortalGodMode());
@@ -29,6 +32,7 @@ public class Mous extends MeteorAddon {
 
     @Override
     public void onRegisterCategories() {
+        LOG.info(LOGPREFIX + " refister categories.....");
         Modules.registerCategory(CATEGORY);
     }
 
